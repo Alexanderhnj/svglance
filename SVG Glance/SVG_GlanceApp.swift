654b65c200b79884bad4a6b92d04fb056554d121
Alexander <Alexanderhnj2001@gmail.com>
2026-06-02 17:34:31 +0200

    Initial Commit

---
+++ SVG Glance/SVG_GlanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVG_GlanceApp.swift
+//  SVG Glance
+//
+//  Created by Alexander Jensen on 02/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SVG_GlanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
